abb6b4096c879be8ec370ca7047cc1322945dba6
beomus <89083094+beomus91@users.noreply.github.com>
2022-02-22 08:45:30 +1000

    change top bar colour

--- config.def.h
+++ config.def.h
@@ -15,11 +15,11 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_Yale[]        = "#004385";
-static const char col_IndigoGrey[]  = "#2A294C";
+static const char col_SlateRed[]    = "#D1615D";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_IndigoGrey,  col_IndigoGrey  },
+	[SchemeSel]  = { col_gray4, col_SlateRed,  col_SlateRed },
 };
 
 /* tagging */
